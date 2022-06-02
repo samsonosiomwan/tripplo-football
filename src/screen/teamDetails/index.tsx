a63e742eb1dcce90c557b0934205715562a9e496
mac <samsonosiomwan@gmail.com>
2022-06-02 23:12:10 +0100

    added scrollable to teamdetails

--- src/screen/teamDetails/index.tsx
+++ src/screen/teamDetails/index.tsx
@@ -1,6 +1,7 @@
 import React, {FC, useLayoutEffect} from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, Platform, StyleSheet, Text, View} from 'react-native';
 import LogoTitle from '../../components/LogoTItle';
+import ScrollableContainer from '../../components/ScrollableContainer';
 import Colors from '../../utils/Colors';
 import Fonts from '../../utils/Fonts';
 
@@ -10,60 +11,68 @@ const TeamDetails: FC<any> = ({route, navigation}) => {
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerTitle: () => <LogoTitle  title={team?.displayName}/>
+      headerTitle: () => <LogoTitle title={team?.displayName} />,
     });
   }, []);
 
   return (
-    <>
-    <View style={styles.container}>
-      <View style={styles.titleWrapper}>
-        <Text style={styles.title}>Team Details</Text>
-      </View>
-          <View >
-              <View style={styles.contentWrapper}>
-                <Text style={styles.content}>name</Text>
-                <Text style={styles.content}>{team.displayName}</Text>
-              </View>
-              <View style={styles.contentWrapper}>
-                <Text style={styles.content}>location</Text>
-                <Text style={styles.content}>{team.location}</Text>
-              </View>
-              <View style={styles.contentWrapper}>
-                <Text style={styles.content}>abbriviation</Text>
-                <Text style={styles.content}>{team.abbreviation}</Text>
-              </View>
-              <View style={styles.contentWrapper}>
-                <Text style={styles.content}>active</Text>
-                <Text style={styles.content}>{team.isActive?"Yes":"No"}</Text>
-              </View>
+    <ScrollableContainer
+      style={styles.scrollableContainer}
+      containerStyle={{flexGrow: 1}}>
+      <View style={styles.container}>
+        <View style={styles.titleWrapper}>
+          <Text style={styles.title}>Team Details</Text>
+        </View>
+        <View>
+          <View style={styles.contentWrapper}>
+            <Text style={styles.content}>name</Text>
+            <Text style={styles.content}>{team.displayName}</Text>
           </View>
-    </View>
-
-    <View style={styles.container}>
-      <View style={styles.titleWrapper}>
-        <Text style={styles.title}>Stats</Text>
+          <View style={styles.contentWrapper}>
+            <Text style={styles.content}>location</Text>
+            <Text style={styles.content}>{team.location}</Text>
+          </View>
+          <View style={styles.contentWrapper}>
+            <Text style={styles.content}>abbriviation</Text>
+            <Text style={styles.content}>{team.abbreviation}</Text>
+          </View>
+          <View style={styles.contentWrapper}>
+            <Text style={styles.content}>active</Text>
+            <Text style={styles.content}>{team.isActive ? 'Yes' : 'No'}</Text>
+          </View>
+        </View>
       </View>
 
-      {stats?.map((item: any, index: number) => {
-        return (
-          <View key={index}>
-            {(item?.name === 'losses' || item?.name === 'wins' || item?.name === 'ties') && (
-              <View style={styles.contentWrapper}>
-                <Text style={styles.content}>{item.name}</Text>
-                <Text style={styles.content}>{item?.value}</Text>
-              </View>
-            )}
-          </View>
-        );
-      })}
-    </View>
-    </>
-   
+      <View style={styles.container}>
+        <View style={styles.titleWrapper}>
+          <Text style={styles.title}>Stats</Text>
+        </View>
+
+        {stats?.map((item: any, index: number) => {
+          return (
+            <View>
+              {(item?.name === 'losses' ||
+                item?.name === 'wins' ||
+                item?.name === 'ties') && (
+                <View style={styles.contentWrapper}>
+                  <Text style={styles.content}>{item.name}</Text>
+                  <Text style={styles.content}>{item?.value}</Text>
+                </View>
+              )}
+            </View>
+          );
+        })}
+      </View>
+    </ScrollableContainer>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollableContainer: {
+    backgroundColor: Colors.textBox,
+    flex: 1,
+    paddingBottom: Platform.OS === 'android' ? Fonts.h(16) : 0,
+  },
   container: {
     borderRadius: 8,
     backgroundColor: Colors.white,
@@ -90,7 +99,7 @@ const styles = StyleSheet.create({
   },
   content: {
     fontSize: Fonts.w(15),
-    textTransform:'capitalize'
+    textTransform: 'capitalize',
   },
 });
 
